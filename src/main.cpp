fe385614a834d0c41fe15b8c1edfd8553337eb00
sparks-alec <sparks.alec@gmail.com>
2026-07-25 00:49:31 -0400

    add MONO20

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@
 #define MAX_CURRENT 24
 #define INTENSITY_SCALE_EXPONENT 1.832
 #define MAX_PWM_GAIN_BEFORE_CURRENT_GAIN 64
+#define PCB_RGB12 0
+#define PCB_MONO20 1
+#define PCB_MONO10 2
+
+#define PCB_MODEL PCB_MONO20
+#define BLACKOUT_IF_NO_DMX false
 
 #define NUM_TLC59711 3
 Adafruit_TLC59711 tlc = Adafruit_TLC59711(NUM_TLC59711, 18, 19);
@@ -26,16 +32,23 @@ void whiteTestFrame();
 void blackoutFrame();
 void dmxDataRecevied(DmxInput* instance);
 
+void regularRGBFrame();
+void regularMonoFrame();
+
 void setup(){
   Serial.begin(9600);
-  pinMode(22, OUTPUT);
+  pinMode(6, OUTPUT);
   pinMode(23, OUTPUT);
   pinMode(24, INPUT_PULLUP);
 
   pinMode(10, INPUT_PULLDOWN);
-  pinMode(8, INPUT_PULLDOWN);
+  pinMode(4, INPUT_PULLDOWN);
+  pinMode(2, INPUT_PULLDOWN);
+  pinMode(0, INPUT_PULLDOWN);
   pinMode(11, INPUT_PULLDOWN);
-  pinMode(9, INPUT_PULLDOWN);
+  pinMode(5, INPUT_PULLDOWN);
+  pinMode(3, INPUT_PULLDOWN);
+  pinMode(1, INPUT_PULLDOWN);
 
   delay(300);
 
@@ -47,14 +60,16 @@ void setup(){
   dmxInput.read_async(buffer, dmxDataRecevied);
 
   for (int i = 0; i < 3; i++){
-    digitalWrite(22, HIGH);
+    digitalWrite(6, HIGH);
     delay(100);
-    digitalWrite(22, LOW);
+    digitalWrite(6, LOW);
     delay(200);
   }
 }
 
-int dipDial = 0;
+int dropIndex = 0;
+int dipDial1 = 0;
+int dipDial2 = 0;
 int addressOffset = 0;
 float brightnessGain = 0;
 int redScale = 255;
@@ -65,16 +80,43 @@ int previousControlValue = 0;
 long lastDMXFrameMillis = 0;
 
 void loop(){
-  uint8_t b1 = digitalRead(10);
-  uint8_t b2 = digitalRead(8);
-  uint8_t b4 = digitalRead(11);
-  uint8_t b8 = digitalRead(9);
-  dipDial = b1 + b2 * 2 + b4 * 4 + b8 * 8; // 0-15
-  addressOffset = 11 * dipDial;
+
+  if(PCB_MODEL == PCB_RGB12){
+    uint8_t b1 = digitalRead(10);
+    uint8_t b2 = digitalRead(8);
+    uint8_t b4 = digitalRead(11);
+    uint8_t b8 = digitalRead(9);
+    dropIndex = b1 + b2 * 2 + b4 * 4 + b8 * 8; // 0-15
+    addressOffset = 11 * dropIndex;
+
+  }else if(PCB_MODEL == PCB_MONO20){
+    uint8_t b1a = digitalRead(10);
+    uint8_t b2a = digitalRead(4);
+    uint8_t b4a = digitalRead(2);
+    uint8_t b8a = digitalRead(0);
+    dipDial1 = b1a + b2a * 2 + b4a * 4 + b8a * 8; // 0-15
+
+    uint8_t b1b = digitalRead(11);
+    uint8_t b2b = digitalRead(5);
+    uint8_t b4b = digitalRead(3);
+    uint8_t b8b = digitalRead(1);
+    dipDial2 = b1b + b2b * 2 + b4b * 4 + b8b * 8; // 0-15
+
+    dropIndex = dipDial1 * 10 + dipDial2;
+    addressOffset = 20 * dropIndex;
+  }
+
+  
 
   controlValue = buffer[511];
 
-  if (dipDial == 15){
+  if(false){
+    allRGBTestFrame();
+
+  }else if (PCB_MODEL == PCB_RGB12 && dropIndex == 15){
+    soloRGBTestFrame();
+
+  }else if (PCB_MODEL == PCB_MONO20 && (dipDial2 == 15 || dipDial1 == 15)){
     soloRGBTestFrame();
     
   }else if(controlValue == 10){
@@ -92,7 +134,7 @@ void loop(){
   }else if(controlValue >= 80 && controlValue <= 94){
     whiteTestFrame();
 
-  }else if(millis() - lastDMXFrameMillis > 3000){
+  }else if(BLACKOUT_IF_NO_DMX && millis() - lastDMXFrameMillis > 3000){
     blackoutFrame();
 
   }else{
@@ -102,15 +144,13 @@ void loop(){
     greenScale = buffer[509];
     blueScale = buffer[510];
 
-    addressOffset *= 3;
+    if(PCB_MODEL == PCB_RGB12){
+      regularRGBFrame();
 
-    for (int i = 0; i < 12; i++){
-      int ledOffset = i * 3;
-      int redVal = constrain(pow(buffer[ledOffset + addressOffset + 1], INTENSITY_SCALE_EXPONENT) * (redScale/255.0) * brightnessGain, 0, 65535);
-      int greenVal = constrain(pow(buffer[ledOffset + addressOffset + 2], INTENSITY_SCALE_EXPONENT)* (greenScale/255.0) * brightnessGain, 0, 65535);
-      int blueVal = constrain(pow(buffer[ledOffset + addressOffset + 3], INTENSITY_SCALE_EXPONENT)* (blueScale/255.0) * brightnessGain, 0, 65535);
+    }else if(PCB_MODEL == PCB_MONO20){
+      regularMonoFrame();
 
-      tlc.setLED(ledPatch[i], redVal, greenVal, blueVal);
+
     }
   }
   tlc.write();
@@ -127,6 +167,37 @@ void loop(){
   previousControlValue = buffer[511];
 
   delay(3);
+}
+
+void regularRGBFrame(){
+  addressOffset *= 3;
+
+  for (int i = 0; i < 12; i++){
+    int ledOffset = i * 3;
+    int redVal = constrain(pow(buffer[ledOffset + addressOffset + 1], INTENSITY_SCALE_EXPONENT) * (redScale/255.0) * brightnessGain, 0, 65535);
+    int greenVal = constrain(pow(buffer[ledOffset + addressOffset + 2], INTENSITY_SCALE_EXPONENT)* (greenScale/255.0) * brightnessGain, 0, 65535);
+    int blueVal = constrain(pow(buffer[ledOffset + addressOffset + 3], INTENSITY_SCALE_EXPONENT)* (blueScale/255.0) * brightnessGain, 0, 65535);
+
+    tlc.setLED(ledPatch[i], redVal, greenVal, blueVal);
+  }
+}
+
+void regularMonoFrame(){
+  int processedValues[20];
+  for (int i = 0; i < 20; i++){
+    processedValues[i] = constrain(buffer[addressOffset+i+1]*255, 0, 65535);
+  }
+
+
+  tlc.setLED(4, processedValues[12], processedValues[14], processedValues[16]); // 12/14/16
+  tlc.setLED(5, processedValues[18], 0, 0); // 18/_/_
+  tlc.setLED(6, 0, 0, processedValues[19]); // _/_/19
+  tlc.setLED(7, processedValues[17], processedValues[15], processedValues[13]); // 17/15/13
+  tlc.setLED(8, processedValues[0], processedValues[2], processedValues[4]); // 0/2/4
+  tlc.setLED(9, processedValues[6], processedValues[8], processedValues[10]); // 6/8/10
+  tlc.setLED(10, processedValues[11], processedValues[9], processedValues[7]); // 11/9/7
+  tlc.setLED(11, processedValues[5], processedValues[3], processedValues[1]); // 5/3/1
+  
 }
 
 void soloRGBTestFrame(){
@@ -189,10 +260,10 @@ void indexTestFrame(){
   }
 
   int val = 20000;
-  if(dipDial < 11){
-    tlc.setLED(ledPatch[dipDial], 0, val, val);
+  if(dropIndex < 11){
+    tlc.setLED(ledPatch[dropIndex], 0, val, val);
   }else{
-    tlc.setLED(ledPatch[dipDial-11], val, 0, val);
+    tlc.setLED(ledPatch[dropIndex-11], val, 0, val);
   } 
 }
 
@@ -218,6 +289,6 @@ void applyBrightness(){
 }
 
 void __isr dmxDataRecevied(DmxInput* instance) {
-  // digitalWrite(22, !digitalRead(22));
+  // digitalWrite(6, !digitalRead(6));
   lastDMXFrameMillis = millis();
 }
